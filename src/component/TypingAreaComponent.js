0de2fa81982413a682928259abcbd0817954411b
Harshit <harshitchugh001@gmail.com>
2023-06-11 00:13:23 +0530

    adding some features

--- src/component/TypingAreaComponent.js
+++ src/component/TypingAreaComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TypingAreaComponent = ({
-  typingGame, 
+  typingGame,
   typedText,
   handleFormSubmit,
   handleTextareaChange,
@@ -9,13 +9,22 @@ const TypingAreaComponent = ({
   handleTextareaBlur,
 }) => {
   const calculateProgress = (currentSentence, typedText) => {
-    const progress = Math.floor(
-      (typedText.length / currentSentence.length) * 100
-    );
+    const progress = Math.floor((typedText.length / currentSentence.length) * 100);
     return progress;
   };
 
-  const progress = calculateProgress(typingGame.currentSentence, typingGame.typedText);
+  const progress = calculateProgress(
+    typingGame.currentSentence,
+    typingGame.typedText
+  );
+
+  const getNextKey = () => {
+    const { currentSentence, typedText } = typingGame;
+    const nextKeyIndex = typedText.length;
+    return currentSentence[nextKeyIndex];
+  };
+
+  const nextKey = getNextKey();
 
   return (
     <div className="input-box">
@@ -23,16 +32,23 @@ const TypingAreaComponent = ({
         <div className="progress-bar-container">
           <progress value={progress} max={100} />
         </div>
-        <textarea
-          value={typedText}
-          onChange={handleTextareaChange}
-          onFocus={handleTextareaFocus}
-          onBlur={handleTextareaBlur}
-          placeholder="Start typing..."
-          rows={4}
-          cols={50}
-        />
-        <button type="submit">Submit</button>
+        <div>
+          <textarea
+            value={typedText}
+            onChange={handleTextareaChange}
+            onFocus={handleTextareaFocus}
+            onBlur={handleTextareaBlur}
+            placeholder="Start typing..."
+            rows={4}
+            cols={50}
+          />
+          <div className="next-key">
+            Next Key: <span className="highlight">{nextKey}</span>
+          </div>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
--- src/component/TypingGame.js
+++ src/component/TypingGame.js
@@ -1,5 +1,4 @@
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import KeyboardComponent from './KeyboardComponent';
 import TypingAreaComponent from './TypingAreaComponent';
@@ -10,27 +9,25 @@ import {
   setEndTime,
   setWPM,
   setAccuracy,
-  
 } from '../redux/typingGameSlice';
 import './main.css';
 
 const sentences = [
-  'Dream big, work hard, achieve greatness.',
-  'Embrace change and unleash your true potential.',
-  'Love yourself, spread kindness, create happiness.',
-  'Believe in yourself and never give up.',
-  'Find joy in the little things every day.',
-  'Chase your dreams with passion and perseverance.',
-  'Celebrate life’s journey with a grateful heart.',
-  'The quick brown fox jumps over the lazy dog.',
-  'Sphinx of black quartz, judge my vow.',
-  'Pack my box with five dozen liquor jugs.',
-  'How vexingly quick daft zebras jump!',
+  "The quick brown fox jumps over the lazy dog. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pretium auctor ipsum nec varius. Phasellus in rhoncus mi. Vivamus eleifend scelerisque ligula, id fringilla nunc vestibulum et. Nulla facilisi. Praesent convallis consequat magna, at lobortis elit consequat id. Sed sagittis tincidunt felis, eu bibendum risus lobortis in.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce posuere volutpat dolor, a sollicitudin elit tincidunt nec. Nunc congue fringilla tellus at ultrices. Suspendisse sed dapibus lacus. Nullam sit amet purus ut lectus tristique tempus. Cras ut justo vel justo venenatis euismod. In hac habitasse platea dictumst.",
+  "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc consectetur congue commodo. Integer pulvinar tincidunt mauris, sed blandit felis commodo et. Nam a varius lectus. Ut ac mauris ligula. Praesent vehicula velit id mauris laoreet, sed consequat dolor malesuada. Duis consequat dui dolor, eu tempor purus pharetra sed.",
+  "Sed non ipsum euismod, faucibus nisi id, lobortis enim. Curabitur blandit lectus in nisi auctor dignissim. Morbi facilisis tortor non elit aliquet ullamcorper. Aenean ac nisl a est vulputate congue eu eget massa. Phasellus viverra congue cursus. Fusce eget ex nec odio gravida malesuada ac sit amet urna.",
+  "Cras pulvinar metus a quam viverra, vitae pulvinar orci auctor. Fusce id malesuada lorem, at efficitur urna. Morbi auctor ultrices tincidunt. Phasellus sed fermentum neque. Fusce feugiat dui sed velit scelerisque, ut suscipit sapien facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+  "Etiam non nisi vitae lacus fringilla dictum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras venenatis nisl a nunc tincidunt, non ultricies tellus semper. Pellentesque id feugiat tortor. In ut dui ut dolor lacinia pellentesque vitae a sem. In hac habitasse platea dictumst.",
+  "Aliquam erat volutpat. Vestibulum maximus, nulla non dignissim fringilla, arcu nunc lacinia sapien, eu commodo orci mi a nulla. Fusce vehicula mi et enim fermentum, non fermentum odio tincidunt. Morbi tincidunt interdum lorem ac venenatis. Pellentesque mattis fringilla est, id fringilla enim semper a. Integer lobortis nibh id ligula accumsan, et faucibus tellus consequat.",
+  "Phasellus accumsan tincidunt sem in aliquam. Nulla faucibus vestibulum ante id ultrices. Maecenas nec erat eget mauris luctus pharetra. Aliquam commodo ultricies nisi, a commodo lectus. Nulla quis massa scelerisque, laoreet erat vel, aliquam lacus. Nullam at lectus vitae purus elementum laoreet. Aliquam tempor odio vel purus lacinia aliquet."
 ];
 
 const TypingGame = () => {
   const dispatch = useDispatch();
   const typingGame = useSelector((state) => state.typingGame);
+  const [keyPressCount, setKeyPressCount] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(300);
 
   const handleTextChange = (button) => {
     const value = button.toUpperCase();
@@ -54,6 +51,8 @@ const TypingGame = () => {
     dispatch(setEndTime(0));
     dispatch(setWPM(0));
     dispatch(setAccuracy(0));
+    setKeyPressCount(0);
+    setRemainingTime(300);
   };
 
   const calculateWPM = (typedWordsCount, minutes) => {
@@ -98,6 +97,43 @@ const TypingGame = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      setKeyPressCount((count) => count + 1);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    let timer = null;
+
+    if (typingGame.startTime) {
+      timer = setInterval(() => {
+        const minutes = (Date.now() - typingGame.startTime) / 60000;
+        const remainingTimeInSeconds = Math.max(0, Math.floor(300 - minutes * 60));
+        setRemainingTime(remainingTimeInSeconds);
+
+        if (remainingTimeInSeconds <= 0) {
+          clearInterval(timer);
+          handleFormSubmit(new Event('submit'));
+        }
+      }, 1000);
+    }
+
+    if (remainingTime === 0) {
+      clearInterval(timer);
+      alert(`Number of Key Presses: ${keyPressCount}`);
+      window.location.reload();
+    }
+
+    return () => clearInterval(timer);
+  }, [typingGame.startTime, handleFormSubmit, remainingTime, keyPressCount]);
+
   return (
     <div className="typing-game-container">
       <div className="content">
@@ -105,21 +141,20 @@ const TypingGame = () => {
         <div className="sentences-box">
           <div className="sentence">
             <p>
-              {typingGame.currentSentence.slice(
-                0,
-                typingGame.typedText.length
-              )}
+              {typingGame.currentSentence.slice(0, typingGame.typedText.length)}
               <span className="remaining-characters">
                 {typingGame.currentSentence.slice(typingGame.typedText.length)}
               </span>
             </p>
           </div>
+          <div className='container '>
           <TypingAreaComponent
             typingGame={typingGame}
             typedText={typingGame.typedText}
             handleFormSubmit={handleFormSubmit}
             handleTextareaChange={(e) => dispatch(setTypedText(e.target.value))}
           />
+          </div>
         </div>
         <div className="features">
           <div className="individual-feature">
@@ -128,14 +163,18 @@ const TypingGame = () => {
           <div className="individual-feature">
             <p>Accuracy: {typingGame.accuracy}%</p>
           </div>
+          <div className="individual-feature">
+            <p>Key Presses: {keyPressCount}</p>
+          </div>
+          <div className="individual-feature">
+            <p>Time Remaining: {remainingTime} seconds</p>
+          </div>
         </div>
         <button onClick={handleSentenceChange}>Change Sentence</button>
       </div>
 
       <div className="keyboard-container keyboard-letter-wrapper">
-      
-
-        <KeyboardComponent  handleTextChange={handleTextChange} />
+        <KeyboardComponent handleTextChange={handleTextChange} />
       </div>
     </div>
   );
